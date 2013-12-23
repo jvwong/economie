7d0f130e0b649da13f15caf458572dc557483919
jvwong <jvwong@outlook.com>
2013-12-22 20:58:27 -0500

    added CRUD

--- static/mousy/js/household.js
+++ static/mousy/js/household.js
@@ -10,7 +10,9 @@
                                     .attr("class", "tooltip")               
                                     .style("opacity", 0);
         var dtgFormat2 = d3.time.format("%a %e %b");
-        
+
+        var monthNames = [ "jan", "feb", "mar", "apr", "may", "jun",
+                            "jul", "aug", "sep", "oct", "nov", "dec" ];
         
        $.ajax({
               type: "GET",
@@ -23,17 +25,27 @@
         
         
         function generate(data) {
-              
-            //console.log(data);
-             
-            var parseDate = d3.time.format("%Y-%b-%d");
-            data.forEach(function(d) {
-                d.date = parseDate.parse(d.date);
-                d.month = d3.time.month(d.date);
-                d.amount = d.amount;
-                d.detail = d.detail;
-                d.who = d.created_by;
-                //console.log(d.date);
+
+
+          var parseDate = d3.time.format("%Y-%b-%d");
+
+          data.forEach(function(d) {
+            d.date = parseDate.parse(d.date);
+            d.year = d.date.getFullYear().toString();
+            d.month = monthNames[d.date.getMonth()];
+            d.day = d.date.getDate().toString();
+            d.amount = d.amount;
+            d.detail = d.detail;
+            d.who = d.created_by;
+            d.pk = d.pk;
+            d.detail_url = ['<a href="',['receipt',d.year,d.month, d.day,d.pk,'">Detail</a>'].join('/')].join('');
+            d.update_url = ['<a href="',['receipt/update',d.pk,'">Update</a>'].join('/')].join('');
+            d.delete_url = ['<a href="',['receipt',d.pk,'delete','">Delete</a>'].join('/')].join('');
+
+//            console.log(d.delete_url);
+//            console.log(d.month);
+//            console.log(d.day);
+
             });
                        
             // Run the data through crossfilter and load our 'facts'
@@ -63,18 +75,21 @@
                        
             // Setup the charts
             // Table of earthquake data
-            dataTable.width(span12_width )
-                .dimension(dayDimension)
+            dataTable.width( span12_width )
+                .dimension( dayDimension )
                 .group(function(d) {
                     return "expenditures";
                 })
                 .size(100)
                 .columns([
-                    function(d) { return d.date.toDateString(); },
-                    function(d) { return d.amount; },
-                    function(d) { return d.detail; },
-                    function(d) { return d.who; }                    
-                ]);
+                  function(d) { return d.date.toDateString(); },
+                  function(d) { return d.amount; },
+                  function(d) { return d.detail; },
+                  function(d) { return d.who; },
+                  function(d) { return d.detail_url;},
+                  function(d) { return d.update_url;},
+                  function(d) { return d.delete_url;}
+              ]);
            
             
             var start_dayChart = d3.time.day.offset(d3.extent(data, function(d){ return d.date; })[0], -1);
